03d611f147a1478606a78c73bb047c7ee2d559cd
David Wang <davidw294@gmail.com>
2015-01-29 20:07:04 -0500

    Jan 29

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
   root "pages#home"
-  get "about" => "pages#about"
+  get "about" => "pages#about" # crates about_path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
